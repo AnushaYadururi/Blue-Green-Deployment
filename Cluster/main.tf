d2e2530e43b8dd27370ca4e1549cd4cc68ffa394
AnushaYadururi <anushayadururi333@gmail.com>
2025-09-03 17:08:47 +0530

    Update main.tf

--- Cluster/main.tf
+++ Cluster/main.tf
@@ -1,7 +1,6 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
 }
-
 resource "aws_vpc" "devopsshack_vpc" {
   cidr_block = "10.0.0.0/16"
 
@@ -14,7 +13,7 @@ resource "aws_subnet" "devopsshack_subnet" {
   count = 2
   vpc_id                  = aws_vpc.devopsshack_vpc.id
   cidr_block              = cidrsubnet(aws_vpc.devopsshack_vpc.cidr_block, 8, count.index)
-  availability_zone       = element(["ap-south-1a", "ap-south-1b"], count.index)
+  availability_zone       = element(["us-east-1a", "us-east-1b"], count.index)
   map_public_ip_on_launch = true
 
   tags = {
